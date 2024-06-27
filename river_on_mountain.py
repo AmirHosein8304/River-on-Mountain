58311d4c5c418aa1970f94e330e1cb2e5813b56f
AmirHosein <ahtavakkoli96@gmail.com>
2024-06-27 18:08:20 +0330

    solve river_finder none problem

--- river_on_mountain.py
+++ river_on_mountain.py
@@ -2,15 +2,19 @@ def river_finder(mountian, x, y,river_direction = [], rivers=[]):
     if x+1<=len(mountain[0]) and mountain[y][x+1]<mountain[y][x] and (x+1,y) not in river_direction:
         river_direction.append((y,x+1))
         river_finder(mountian, x+1, y, river_direction)
+        return rivers
     elif x-1>=0 and mountain[y][x-1]<mountain[y][x] and (x-1,y) not in river_direction:
         river_direction.append((y,x-1))
         river_finder(mountian, x-1, y, river_direction)
+        return rivers
     elif y+1<=len(mountain) and mountain[y+1][x]<mountain[y][x] and (x,y+1) not in river_direction:
         river_direction.append((y+1,x))
         river_finder(mountian, x, y+1, river_direction)
+        return rivers
     elif y-1>=0 and mountain[y-1][x]<mountain[y][x] and (x,y-1) not in river_direction:
         river_direction.append((y-1,x))
         river_finder(mountian, x, y-1, river_direction)
+        return rivers
     else:
         rivers.append(river_direction)
         river_direction=[]
