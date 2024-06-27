aa80c72bcde715af87f3ee713e5cd329a93b70d5
AmirHosein <ahtavakkoli96@gmail.com>
2024-06-27 17:45:55 +0330

    last version on 6 tir

--- river_on_mountain.py
+++ river_on_mountain.py
@@ -1,21 +1,21 @@
-def river_finder(mountian, x, y,river_direction = [],vis=[]):
+def river_finder(mountian, x, y,river_direction = [], rivers=[]):
     if x+1<=len(mountain[0]) and mountain[y][x+1]<mountain[y][x] and (x+1,y) not in river_direction:
-        river_direction.append((x+1,y))
-        river_finder(mountian, x+1, y, river_direction,vis)
+        river_direction.append((y,x+1))
+        river_finder(mountian, x+1, y, river_direction)
     elif x-1>=0 and mountain[y][x-1]<mountain[y][x] and (x-1,y) not in river_direction:
-        river_direction.append((x-1,y))
-        river_finder(mountian, x-1, y, river_direction,vis)
+        river_direction.append((y,x-1))
+        river_finder(mountian, x-1, y, river_direction)
     elif y+1<=len(mountain) and mountain[y+1][x]<mountain[y][x] and (x,y+1) not in river_direction:
-        river_direction.append((x,y+1))
-        river_finder(mountian, x, y+1, river_direction,vis)
+        river_direction.append((y+1,x))
+        river_finder(mountian, x, y+1, river_direction)
     elif y-1>=0 and mountain[y-1][x]<mountain[y][x] and (x,y-1) not in river_direction:
-        river_direction.append((x,y-1))
-        river_finder(mountian, x, y-1, river_direction,vis)
+        river_direction.append((y-1,x))
+        river_finder(mountian, x, y-1, river_direction)
     else:
-        return 'hi'
+        rivers.append(river_direction)
+        river_direction=[]
+        return rivers
 n, m = map(int, input().split())
-x, y = map(int, input().split())
+y, x = map(int, input().split())
 mountain = [list(map(int, input().split())) for i in range(n)]
-print(mountain)
-print('####################################')
-print(river_finder(mountain,x,y,[(x,y)]))
+print(river_finder(mountain,x,y,[(y,x)]))
