837766ff6c99736e1e66bdfa5ec9828bf7623bcb
AmirHosein <ahtavakkoli96@gmail.com>
2024-06-27 19:51:17 +0330

    last commit on 7 tir

--- new_river.py
+++ new_river.py
@@ -1,27 +1,27 @@
 all_rivers = []
-def river_finder(mountian, x, y,river_direction = [],flag=0,flag_1=False,counter=0):
+def river_finder(mountian, x, y,river_direction,flag=0,flag_1=False,counter=0):
     counter+=1
-    if x+1<=len(mountain[0]) and mountain[y][x+1]<mountain[y][x] and (x+1,y) not in river_direction:
+    if x+1<=len(mountain[0])-1 and mountain[y][x+1]<mountain[y][x] and (y,x+1) not in river_direction:
         river_direction.append((y,x+1))
         river_finder(mountian, x+1, y, river_direction,flag,flag_1,counter)
         river_direction=[]
         flag+=1
         flag_1=True
-    if x-1>=0 and mountain[y][x-1]<mountain[y][x] and (x-1,y) not in river_direction:
+    if x-1>=0 and mountain[y][x-1]<mountain[y][x] and (y,x-1) not in river_direction:
         river_direction.append((y,x-1))
         river_finder(mountian, x-1, y, river_direction,flag,flag_1,counter)
         river_direction=[]
         if not flag_1:
             flag+=1
             flag_1=True
-    if y+1<=len(mountain) and mountain[y+1][x]<mountain[y][x] and (x,y+1) not in river_direction:
+    if y+1<=len(mountain)-1 and mountain[y+1][x]<mountain[y][x] and (y+1,x) not in river_direction:
         river_direction.append((y+1,x))
         river_finder(mountian, x, y+1, river_direction,flag,flag_1,counter)
         river_direction=[]
         if not flag_1:
             flag+=1
             flag_1=True
-    if y-1>=0 and mountain[y-1][x]<mountain[y][x] and (x,y-1) not in river_direction:
+    if y-1>=0 and mountain[y-1][x]<mountain[y][x] and (y-1,x) not in river_direction:
         river_direction.append((y-1,x))
         river_finder(mountian, x, y-1, river_direction,flag,flag_1,counter)
         river_direction=[]
